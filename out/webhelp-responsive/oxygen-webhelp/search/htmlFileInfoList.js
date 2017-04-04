1cbe43a0619b0f61ebab446a0fb15495364a811d
Caroline Frulla <caroli9@vt.edu>
2017-04-04 18:27:14 -0400

    Updated output

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -1,6 +1,8 @@
 fil = new Array();
-fil["0"]= "c-breakfast.html@@@Breakfast@@@With these recipes, you can quickly make your own delicious breakfast! You are welcome to all of the materials in my kitchen...";
-fil["1"]= "t-burrito.html@@@Burrito Recipe@@@These burritos make for a quick breakfast you can eat on the go!...";
-fil["2"]= "t-frenchtoast.html@@@French Toast Recipe@@@This french toast can be paired with toppings of your choice, such as fruit...";
-fil["3"]= "t-mcfitfun.html@@@McFitFun Recipe@@@This English muffin recipie is a healthier at-home version of McDonalds Egg McMuffin!...";
-fil["4"]= "t-pancakes.html@@@Pancakes Recipe@@@These pancakes can be paired with toppings of your choice, such as fruit...";
+fil["0"]= "c-breakfast.html@@@Welcome to Your New Recipe Book!@@@These recipes were made with you in mind, and with them, you can quickly make your own delicious breakfast! You are welcome to all of the utensils and ingredients in my kitchen, just don&apos;t leve a mess...";
+fil["1"]= "c-references.html@@@More Recipes@@@If you enjoyed any of the recipes I provided for you, and would like to find more by the same chef, check out their websites for more recipes!...";
+fil["2"]= "c-spoiled.html@@@Spoiled Food@@@This topic lets my guests know when the food I have in the fridge is expected to go bad...";
+fil["3"]= "t-burrito.html@@@Burrito Recipe@@@These burritos make for a quick breakfast you can eat on the go!...";
+fil["4"]= "t-frenchtoast.html@@@French Toast Recipe@@@This french toast can be paired with toppings of your choice, such as fruit...";
+fil["5"]= "t-mcfitfun.html@@@McFitFun Recipe@@@This English muffin recipie is a healthier at-home version of McDonalds Egg McMuffin!...";
+fil["6"]= "t-pancakes.html@@@Pancakes Recipe@@@These pancakes can be paired with toppings of your choice, such as fruit...";
--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -1,8 +1,10 @@
 //List of files which are indexed.
 fl = new Array();
 fl["0"]= "c-breakfast.html";
-fl["1"]= "t-burrito.html";
-fl["2"]= "t-frenchtoast.html";
-fl["3"]= "t-mcfitfun.html";
-fl["4"]= "t-pancakes.html";
+fl["1"]= "c-references.html";
+fl["2"]= "c-spoiled.html";
+fl["3"]= "t-burrito.html";
+fl["4"]= "t-frenchtoast.html";
+fl["5"]= "t-mcfitfun.html";
+fl["6"]= "t-pancakes.html";
 var doStem = false;searchLoaded = true;
